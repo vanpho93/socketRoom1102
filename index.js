88fa70d0d365391c9f82eb361ae42f2018a08708
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-15 13:44:26 +0700

    setup socket iu

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+server.listen(process.env.PORT || 3000, () => console.log('server started'));
+
+app.get('/', (req, res) => res.render('home'));
+
+io.on('connection', socket => {
+    console.log(socket.id);
+});
+
+//emit, on. on
